0cd64c0f92f9085129431ff565f3550e4ed37715
jamespatrickmanning <james.manning@noaa.gov>
2018-03-19 13:15:38 -0400

    added space after "ylb"

--- arduino_ap3-temp.ino
+++ arduino_ap3-temp.ino
@@ -141,7 +141,7 @@ void ap3transmes(String message) {
   
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   //mySerial.write("!!!sudo:o00200000>>");  
-  String mess = String("ylb"+message);
+  String mess = String("ylb "+message); // JiM just added a space after "ylb" as required by AP3 after Jan 2018
   mySerial.print(mess);
   mySerial.print("\n");
   delay(5000);                       // wait for a second
